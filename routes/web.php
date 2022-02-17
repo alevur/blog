5f45b6e7378d1fcef3a2b85cd693bcd2bdca124c
Alevur <alevure@yandex.ru>
2022-02-17 15:42:05 +0400

    add asset path

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace' => 'Main'], function () {
+    Route::get('/', 'IndexController');
 });
 
-Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Auth::routes();
